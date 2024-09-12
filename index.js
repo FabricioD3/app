a3781dcecf8d80cfa29670468e7e6c58c66986d7
Fabricio <oliveirafabricio419@gmail.com>
2024-09-12 07:41:04 -0300

    adiciona inquirer

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+const start = () => {
+    
+    while(true){
+        let opcao = "sair"
+        switch (opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                return
+        }
+    }
+}
+
+start()
